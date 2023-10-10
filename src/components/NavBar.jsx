5487810d325fa5e4c1a2d176850bb510dbeaf710
Hung-En Liao <Hung-En Liao>
2023-10-10 12:54:29 -0300

    navbar components

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleClick = () => {
+    setNav(!nav);
+  };
+  return (
+    <div className="fixed w-full h-[80px] flex items-center px-4 bg-orange-500">
+      <div>
+        <img src="" alt="" />
+      </div>
+      {/* menu */}
+      <div>
+        <ui className="hidden md:flex list-none text-black text-2xl">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Works</li>
+          <li>Contact</li>
+        </ui>
+      </div>
+
+      {/*Hamburger */}
+      <div onClick={handleClick} className="md:hidden">
+        {nav === false ? <FaBars className="text-2xl" /> : <FaTimes />}
+      </div>
+
+      {/*Mobile Menu */}
+
+      <ui
+        className={
+          nav === false
+            ? "hidden"
+            : "absolute top-0 left-0 h-screen w-full flex flex-col items-center justify-center bg-slate-900 list-none"
+        }
+      >
+        <li className="py-6 text-4xl text-white">Home</li>
+        <li className="py-6 text-4xl text-white">About</li>
+        <li className="py-6 text-4xl text-white">Skills</li>
+        <li className="py-6 text-4xl text-white">Works</li>
+        <li className="py-6 text-4xl text-white">Contact</li>
+      </ui>
+    </div>
+  );
+};
+
+export default NavBar;
